09ca4719e1a07ffa871b5d02c244afff34230f6f
Guido Van Hoof <guido.van.hoof3@gmail.com>
2022-03-26 17:47:00 +0100

    Initial Project Setup

---
+++ src/main/java/be/gvh/mvh/MvhApplication.java
@@ -0,0 +1,13 @@
+package be.gvh.mvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/gvh/mvh/MvhApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gvh.mvh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
